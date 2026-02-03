f9d90638a69fc3ad8ed80547ebe0a1f957276281
SimplyAtoma <h.rodri831@gmail.com>
2026-02-02 21:57:42 -0800

    paddle input

---
+++ Assets/Scripts/PaddleController.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PaddleController : MonoBehaviour
+{
+    public float speed = 0;
+    private Rigidbody rb;
+    private float movementY = 0;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        
+    }
+
+    public void OnMove(InputValue movementValue)
+    {
+        Vector2 movementVector = movementValue.Get<Vector2>();
+        movementY = movementVector.y;
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        Vector3 movement = new Vector3(0.0f, movementY, 0.0f);
+        rb.AddForce(movement * speed);
+    }
+}
