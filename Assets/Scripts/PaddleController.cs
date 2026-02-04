139e1cb39050516fbd8f5f3de3783a7c070cadaf
SimplyAtoma <h.rodri831@gmail.com>
2026-02-03 23:13:06 -0800

    fixed paddle

--- Assets/Scripts/PaddleController.cs
+++ Assets/Scripts/PaddleController.cs
@@ -3,26 +3,50 @@ using UnityEngine.InputSystem;
 
 public class PaddleController : MonoBehaviour
 {
-    public float speed = 0;
+    [Header("Input Actions")]
+    public InputActionAsset actions;
+    public string actionMapName = "Gameplay";
+
+    [Tooltip("Set to P1Move or P2Move per paddle")]
+    public string moveActionName = "P1Move";
+
+    [Header("Movement")]
+    public float moveSpeed = 8f;
+    public float yLimit = 4f;
+
     private Rigidbody rb;
-    private float movementY = 0;
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    private InputAction moveAction;
+
+    void Awake()
     {
         rb = GetComponent<Rigidbody>();
-        
+        rb.useGravity = false;
+        rb.constraints = RigidbodyConstraints.FreezePositionX |
+                         RigidbodyConstraints.FreezePositionZ |
+                         RigidbodyConstraints.FreezeRotation;
     }
 
-    public void OnMove(InputValue movementValue)
+    void OnEnable()
     {
-        Vector2 movementVector = movementValue.Get<Vector2>();
-        movementY = movementVector.x;
+        var map = actions.FindActionMap(actionMapName, true);
+        moveAction = map.FindAction(moveActionName, true);
+        moveAction.Enable();
     }
 
-    // Update is called once per frame
+    void OnDisable()
+    {
+        if (moveAction != null) moveAction.Disable();
+    }
+
     void FixedUpdate()
     {
-        Vector3 movement = new Vector3(0.0f, movementY, 0.0f);
-        rb.AddForce(movement * speed);
+        float input = moveAction.ReadValue<float>();
+
+        rb.linearVelocity = new Vector3(0f, input * moveSpeed, 0f);
+
+        var p = transform.position;
+        p.y = Mathf.Clamp(p.y, -yLimit, yLimit);
+        p.z = 0f;
+        transform.position = p;
     }
 }
